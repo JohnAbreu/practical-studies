ffb497dba999f9960d10773fc5fde943886176a0
John Abreu <johnabreu.04@hotmail.com>
2022-06-17 16:26:20 -0400

    este es el ultimo commit de este proyecto (borrar proyecto de practica).

--- figuras.js
+++ figuras.js
@@ -1,1 +1,17 @@
-console.log('klk');
+function ClearImput()
+{
+    console.group("Practicando");  
+    let field = document.getElementById("textbox");
+    console.log(`valor limpiado: ${field.value}`);
+    field.value = "";
+    console.groupEnd();   
+}
+
+function CalcularPrecioDescuento()
+{
+    let  precio = document.getElementById("precio").value;
+    let  descuento = document.getElementById("descuento").value;
+    let PrecioDescuento = document.getElementById("preciodescuento");
+    PrecioDescuento.innerHTML = precio * (100 -descuento)/100;
+    return PrecioDescuento;
+}
